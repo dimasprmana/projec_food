8e1bd775dee5433028dfcc965bf308e9bf50faa7
dimasprmana <dimasprmana>
2024-11-10 13:22:53 +0700

    baru

--- app/Http/Controllers/Client/RestaurantController.php
+++ app/Http/Controllers/Client/RestaurantController.php
@@ -4,43 +4,49 @@ namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth; 
+use Illuminate\Support\Facades\Auth;
 use App\Models\Category;
 use Intervention\Image\ImageManager;
-use Intervention\Image\Drivers\Gd\Driver; 
+use Intervention\Image\Drivers\Gd\Driver;
 use App\Models\Menu;
 use App\Models\Product;
 use App\Models\City;
+use Haruncpi\LaravelIdGenerator\IdGenerator;
+use Carbon\Carbon;
+use App\Models\Gllery;
 
 class RestaurantController extends Controller
 {
-    public function AllMenu(){
+    public function AllMenu()
+    {
         $menu = Menu::latest()->get();
         return view('client.backend.menu.all_menu', compact('menu'));
-    } 
+    }
     // End Method 
 
-    public function AddMenu(){
-       
+    public function AddMenu()
+    {
+
         return view('client.backend.menu.add_menu');
-    } 
+    }
     // End Method 
 
-    public function StoreMenu(Request $request){
+    public function StoreMenu(Request $request)
+    {
 
         if ($request->file('image')) {
             $image = $request->file('image');
             $manager = new ImageManager(new Driver());
-            $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+            $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
             $img = $manager->read($image);
-            $img->resize(300,300)->save(public_path('upload/menu/'.$name_gen));
-            $save_url = 'upload/menu/'.$name_gen;
+            $img->resize(300, 300)->save(public_path('upload/menu/' . $name_gen));
+            $save_url = 'upload/menu/' . $name_gen;
 
             Menu::create([
                 'menu_name' => $request->menu_name,
-                'image' => $save_url, 
-            ]); 
-        } 
+                'image' => $save_url,
+            ]);
+        }
 
         $notification = array(
             'message' => 'Menu Inserted Successfully',
@@ -48,56 +54,59 @@ class RestaurantController extends Controller
         );
 
         return redirect()->route('all.menu')->with($notification);
-                   
+
     }
     // End Method 
-    
-    public function EditMenu($id){
+
+    public function EditMenu($id)
+    {
         $menu = Menu::find($id);
         return view('client.backend.menu.edit_menu', compact('menu'));
     }
-     // End Method 
+    // End Method 
 
-     public function UpdateMenu(Request $request){
+    public function UpdateMenu(Request $request)
+    {
 
         $menu_id = $request->id;
 
         if ($request->file('image')) {
             $image = $request->file('image');
             $manager = new ImageManager(new Driver());
-            $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+            $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
             $img = $manager->read($image);
-            $img->resize(300,300)->save(public_path('upload/menu/'.$name_gen));
-            $save_url = 'upload/menu/'.$name_gen;
+            $img->resize(300, 300)->save(public_path('upload/menu/' . $name_gen));
+            $save_url = 'upload/menu/' . $name_gen;
 
             Menu::find($menu_id)->update([
                 'menu_name' => $request->menu_name,
-                'image' => $save_url, 
-            ]); 
+                'image' => $save_url,
+            ]);
             $notification = array(
                 'message' => 'Menu Updated Successfully',
                 'alert-type' => 'success'
             );
-    
+
             return redirect()->route('all.menu')->with($notification);
 
         } else {
 
             Menu::find($menu_id)->update([
-                'menu_name' => $request->menu_name, 
-            ]); 
+                'menu_name' => $request->menu_name,
+            ]);
             $notification = array(
                 'message' => 'Menu Updated Successfully',
                 'alert-type' => 'success'
             );
-    
+
             return redirect()->route('all.menu')->with($notification);
 
         }
- 
+
     }
     // End Method 
-    public function DeleteMenu($id){
+    public function DeleteMenu($id)
+    {
         $item = Menu::find($id);
         $img = $item->image;
         unlink($img);
@@ -116,22 +125,79 @@ class RestaurantController extends Controller
 
     ////// All Product Method started 
 
-    public function AllProduct(){
+    public function AllProduct()
+    {
         $product = Product::latest()->get();
         return view('client.backend.product.all_product', compact('product'));
-    } 
+    }
     // End Method 
 
-    public function AddProduct(){
+    public function AddProduct()
+    {
         $category = Category::latest()->get();
         $city = City::latest()->get();
         $menu = Menu::latest()->get();
-        return view('client.backend.product.add_product', compact('category','city','menu'));
-    } 
+        return view('client.backend.product.add_product', compact('category', 'city', 'menu'));
+    }
     // End Method 
 
-    public function StoreProduct(){
+    public function StoreProduct(Request $request)
+    {
+
+        $pcode = IdGenerator::generate(['table' => 'products', 'field' => 'code', 'length' => 5, 'prefix' => 'PC']);
+
+        if ($request->file('image')) {
+            $image = $request->file('image');
+            $manager = new ImageManager(new Driver());
+            $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+            $img = $manager->read($image);
+            $img->resize(300, 300)->save(public_path('upload/product/' . $name_gen));
+            $save_url = 'upload/product/' . $name_gen;
+
+            Product::create([
+                'name' => $request->name,
+                'slug' => strtolower(str_replace(' ', '-', $request->name)),
+                'category_id' => $request->category_id,
+                'city_id' => $request->city_id,
+                'menu_id' => $request->menu_id,
+                'code' => $pcode,
+                'qty' => $request->qty,
+                'size' => $request->size,
+                'price' => $request->price,
+                'discount_price' => $request->discount_price,
+                'client_id' => Auth::guard('client')->id(),
+                'most_populer' => $request->most_populer,
+                'best_seller' => $request->best_seller,
+                'status' => 1,
+                'created_at' => Carbon::now(),
+                'image' => $save_url,
+            ]);
+        }
+
+        $notification = array(
+            'message' => 'Product Inserted Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('all.product')->with($notification);
+
     }
-     // End Method 
+    // End Method 
+
+    public function ChangeStatus(Request $request)
+    {
+        $product = Product::find($request->product_id);
+        $product->status = $request->status;
+        $product->save();
+        return response()->json(['success' => 'Status Change Successfully']);
+    }
+    // End Method
+
+     /////////// All Gallery Method Start 
+     public function AllGallery(){
+        $gallery = Gllery::latest()->get();
+        return view('client.backend.gallery.all_gallery', compact('gallery'));
+    } 
+    // End Method 
 
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,10 +1,15 @@
 <?php
 
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
     protected $guarded = [];
+
+    public function menu(){
+        return $this->belongsTo(Menu::class, 'menu_id','id');
+    }
+
+
 }
